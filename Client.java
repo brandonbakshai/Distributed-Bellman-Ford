c76bfa88ca96deb4828dc511f53d7c3b20d94626
brandonbakhshai <brandon.bakhshai@columbia.edu>
2015-11-23 11:23:40 -0500

    adding multiple threads

--- Client.java
+++ Client.java
@@ -1,71 +1,121 @@
 import java.util.*;
 import java.net.*;
+import java.io.*;
 import java.io.IOException;
 
 /**
  * Client class
  */
-public class Client implements Runnable {
+public class Client {
 
-    public static void setup() throws IOException {
-        DatagramSocket send = new DatagramSocket();
-        send.setReuseAddress(true);
-        DatagramPacket packet = new DatagramPacket(new byte[1000], 1000);
-        InetSocketAddress sockAddr = new InetSocketAddress("tokyo.clic.cs.columbia.edu", 9998);
+    DatagramSocket sendSock;
+    DatagramSocket listen;
+    DatagramPacket sendPack;
+    InetSocketAddress sockAddr;
+    static int TIMEOUT = 1000*1000;
+    boolean up2date;
 
-        while (true) {
-            Scanner scanner = new Scanner(System.in);
-            packet.setData(scanner.next().getBytes());
-            packet.setSocketAddress(sockAddr);
-            send.send(packet);
-        }
-    }
+    public void setup(String host, int port) throws IOException {
+        sendSock = new DatagramSocket();
+        sendSock.setReuseAddress(true);
+        sendPack = new DatagramPacket(new byte[1000], 1000);
+        sockAddr = new InetSocketAddress(host, port);
 
+     }
     
-    public void run() {
-        try { setup(); }
-        catch (IOException e) { e.printStackTrace(); }
+    public void sendChanges(String host, int port) throws IOException {
+        setup(host, port);
+        sendPack.setData("There was a change!".getBytes());
+        sendPack.setSocketAddress(sockAddr);
+        sendSock.send(sendPack);
+        up2date = true;
     }
 
-    // class to listen for distance vector updates
-    public static class Worker implements Runnable {
+    // class to check standard input for commands
+    public class Command implements Runnable {
 
-        public static void setup() throws IOException {
-            DatagramSocket listen = new DatagramSocket(9997);
-            listen.setReuseAddress(true);
-            
-            while (true) {
-                byte[] buf = new byte[1000];
-                DatagramPacket packet = new DatagramPacket(buf, buf.length);
-                listen.receive(packet);
-                System.out.println(new String(packet.getData()));
+        public void checkIn() throws IOException {
+            InputStreamReader in = new InputStreamReader(System.in);
+            char[] buf = new char[1000];
+            while(in.read(buf, 0, 1000) > 0) {
+                up2date = false;
+                System.out.println(up2date);
             }
         }
 
+        public void run() {
+            while (true) {
+                System.out.println("Command");
+                try { checkIn(); }
+                catch (IOException e) {e.printStackTrace(); }
+            }
+        }
+    }
+
+
+    // class to listen for distance vector updates from neighbors
+    public class Listen implements Runnable {
+
+        public void setup() throws IOException {
+            listen = new DatagramSocket(9997);
+            listen.setReuseAddress(true);
+
+            byte[] buf = new byte[1000];
+            DatagramPacket packet = new DatagramPacket(buf, buf.length);
+            listen.receive(packet);
+
+            up2date = false;
+
+            System.out.println(up2date + " " + new String(packet.getData()));
+        }
         
         public void run() {
-            try { setup(); }
-            catch (IOException e) { e.printStackTrace(); }
+            try { 
+                while (true) {
+                    System.out.println("Listen");
+                    setup();
+                }
+            } catch (IOException e) { e.printStackTrace(); }
         }
 
     }
 
     // class to represent neighbors in distance vector
-    public static class Neighbor {
+    public class Neighbor {}
 
-
-    }
-
-    public static void main(String[] args) throws SocketException
+    public static void main(String[] args) throws IOException
     {
-        
-        Worker worker = new Worker();
-        Thread threadWorker = new Thread(worker);
+        if (args.length < 2) {
+            System.out.println("usage: java Client <remote_host> <remote_port>");
+            System.exit(1);
+        }
 
         Client client = new Client();
-        Thread threadClient = new Thread(client);
+        client.up2date = true;
 
-        // threadWorker.start();
-        threadClient.start();
+        // initiate neighbor listening worker
+        Listen listen = client.new Listen();
+        Thread listenThread = new Thread(listen);
+
+        // initiate sdin listening worker
+        Command command = client.new Command();
+        Thread commandThread = new Thread(command);
+
+        listenThread.start();
+        commandThread.start();
+
+        while (true) {
+
+            System.out.println("starting inf loop");
+        
+            while (client.up2date)
+                System.out.println(client.up2date);
+
+            System.out.println("broke out of up2date loop");
+
+            // client.listen.close();
+            client.sendChanges(args[0], Integer.parseInt(args[1]));
+        }
+    
     }
 }
