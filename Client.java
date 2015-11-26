745b68a5e694d5ef50adc3d30388837d498a95a4
brandonbakhshai <brandon.bakhshai@columbia.edu>
2015-11-26 12:46:56 -0500

    moved send() from main class to subclass

--- Client.java
+++ Client.java
@@ -3,10 +3,21 @@ import java.net.*;
 import java.io.*;
 import java.io.IOException;
 
+import java.awt.*;
+import java.awt.event.*;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+
+
 /**
  * Client class
  */
-public class Client {
+public class Client extends JFrame {
 
     DatagramSocket sendSock;
     DatagramSocket listenSock;
@@ -15,42 +26,62 @@ public class Client {
     InetSocketAddress sockAddr;
     static int TIMEOUT = 1000*1000;
     boolean up2date = true;
+    JTextArea out;
 
-    public void sendChanges(String host, int port) throws IOException {
-        sendSock = new DatagramSocket();
-        sendSock.setReuseAddress(true);
-        // sendPack = new DatagramPacket(new byte[1000], 1000);
-        String data = "There was a change!";
-        sendPack = new DatagramPacket(data.getBytes(), data.getBytes().length);
-        sockAddr = new InetSocketAddress(host, port);
-
-        System.out.println(new String(sendPack.getData()));
-        sendPack.setSocketAddress(sockAddr);
-        sendSock.send(sendPack);
-        
-        sendSock.close();
-        up2date = true;
-    }
-
-    // class to check standard input for commands
-    public class Command implements Runnable {
-
-        public void checkIn() throws IOException {
-            InputStreamReader in = new InputStreamReader(System.in);
-            char[] buf = new char[1000];
-            while(in.read(buf, 0, 1000) > 0) {
-                System.err.println("Command updated to up2date = " + up2date);
-                up2date = false;
-                System.err.println("Command updated to up2date = " + up2date);
-            }
+    // class to listen for commands
+    // when a command is heard, a new DV will be sent to all neighbors and
+    // the listen socket will be closed thereby restarting the listen thread
+    public class Command extends JFrame implements Runnable, ActionListener {
+    
+        public Command() 
+        {
+            Container cp = getContentPane();
+            cp.setLayout(new GridLayout());
+            JTextArea field;
+            JButton submit;
+     
+            submit = new JButton("Submit"); 
+            field = new JTextArea("");
+            field.setEditable(true);
+            cp.add(field);
+            cp.add(submit);
+            submit.addActionListener(this);
+            field.setMaximumSize(getMaximumSize());
+            JScrollPane scroll = new JScrollPane (field, 
+                JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+            cp.add(scroll);
+            out = field;
+          
+            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  // Exit program if close-window button clicked
+            setTitle("DV program"); // "this" JFrame sets title
+            setSize(300, 300);         // "this" JFrame sets initial size
+            setVisible(true);
         }
 
+        public void sendChanges(String host, int port) throws IOException {
+            sendSock = new DatagramSocket();
+            sendSock.setReuseAddress(true);
+            // sendPack = new DatagramPacket(new byte[1000], 1000);
+            String data = "There was a change!";
+            sendPack = new DatagramPacket(data.getBytes(), data.getBytes().length);
+            sockAddr = new InetSocketAddress(host, port);
+
+            System.out.println(new String(sendPack.getData()));
+            sendPack.setSocketAddress(sockAddr);
+            sendSock.send(sendPack);
+        
+            sendSock.close();
+        }
+
+        public void actionPerformed(ActionEvent e) {
+            try { sendChanges("athens.clic.cs.columbia.edu", 21344); }
+            catch (IOException err) { err.printStackTrace(); }
+            listenSock.close();
+            out.setText(out.getText() + "\n" + "sent");
+        }
+        
         public void run() {
-            while (true) {
-                System.out.println("Command");
-                try { checkIn(); }
-                catch (IOException e) {e.printStackTrace(); }
-            }
+            System.out.println("Command");
         }
     }
 
@@ -65,21 +96,18 @@ public class Client {
                       {
                           listenSock = new DatagramSocket(21344); 
                           listenSock.setReuseAddress(true);
-                          up2date = true;
                       }
                 } // establish and bind socket
                 catch (IOException e) { e.printStackTrace(); }
-                while (up2date) {
-                    try {
-                        listenSock.setSoTimeout(30 * 1000); // timeout for 30 seconds
-                        listenSock.setReuseAddress(true);
-                        listenPack = new DatagramPacket(new byte[100], 100);
-                        System.err.println("Socket listening on port " + listenSock.getLocalPort());
-                        listenSock.receive(listenPack);
-                        System.err.println("Information received.");
-                        System.err.println(new String(listenPack.getData()));
-                    } catch (IOException e) { up2date = false; } // if sock timeouts, set var equal to false
-                }
+                try {
+                    listenSock.setSoTimeout(30 * 1000); // timeout for 30 seconds
+                    listenSock.setReuseAddress(true);
+                    listenPack = new DatagramPacket(new byte[100], 100);
+                    System.err.println("Socket listening on port " + listenSock.getLocalPort());
+                    listenSock.receive(listenPack);
+                    System.err.println("Information received.");
+                    System.err.println(new String(listenPack.getData()));
+                } catch (IOException e) { ; } // if sock timeouts, set var equal to false
             }
         }        
     }
@@ -118,8 +146,6 @@ public class Client {
             System.exit(1);
         }
 
-        PrintWriter writer = new PrintWriter("./output.txt", "UTF-8");
-        
         Client client = new Client();
 
         // initiate neighbor listening worker
@@ -131,21 +157,6 @@ public class Client {
         Thread commandThread = new Thread(command);
 
         listenThread.start();
-        // commandThread.start();
-
-        while (true) {
-
-            System.out.println("starting inf loop");
-        
-            int i = 0;
-            while (client.up2date)
-                Thread.sleep(100);
-
-            System.err.println("broke out of up2date loop");
-
-            client.listenSock.close();
-            client.sendChanges(args[0], Integer.parseInt(args[1]));
-        }
-    
+        commandThread.start();
     }
 }
